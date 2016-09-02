2e4bf6d3db9924b045f1eab23a7c3b7340cd7761
Shengyu Zhang <lastavengers@outlook.com>
2016-09-03 00:56:36 +0800

    Update, thx @pandada8

--- log/web/web.py
+++ log/web/web.py
@@ -8,50 +8,93 @@ from tornado.httpserver import HTTPServer
 class IndexHandler(tornado.web.RequestHandler):
     chans = []
 
-    def initialize(self, chans):
-        self.chans = chans
+    def initialize(self, ):
+        self.chans = self.application.chans
 
     def get(self):
         links = []
+
         for chan in self.chans:
-            links.append((chan, self.reverse_url(chan, 'today')))
+            # TODO: concat url: is it right?
+            links.append((chan, '/' + chan + '/today'))
+
         self.render('index.html', 
                 title = 'IRC Log Index',
-                subtitle = strftime('%Y-%m-%d'),
+                subtitle = '%s channel(s)' % len(links),
                 links = links)
 
-class ChanLogHandler(tornado.web.RequestHandler):
-    name = ''
+
+class ChanIndexHandler(tornado.web.RequestHandler):
     path = ''
 
-    def initialize(self, name, path):
-        self.name = name
-        self.path = path
+    def initialize(self):
+        self.path = self.application.path
 
-    def get(self, date):
+    def get(self, chan):
+        links = []
+        try:
+            files = os.listdir(os.path.join(self.path, chan))
+        except FileNotFoundError:
+            raise tornado.web.HTTPError(404)
+
+        for f in files:
+            # TODO: concat url: is it right?
+            links.append((os.path.splitext(f)[0], '/' + chan + '/' + os.path.splitext(f)[0]))
+
+        self.render('logs.html', 
+                title = self.path_args[0],
+                subtitle = 'index',
+                links = links)
+
+    def write_error(self, status_code, **kwargs):
+        if status_code == 404:
+            self.render('404.html', 
+                    title = 'IRC Log Index',
+                    subtitle = self.path_args[0],
+                    msg = 'No such channel.',
+                    )
+
+class ChanLogHandler(tornado.web.RequestHandler):
+    path = ''
+
+    def initialize(self):
+        self.path = self.application.path
+
+
+    def get(self, chan, date):
         if date == 'today':
             date = strftime('%Y-%m-%d')
+
         logs = []
-        fname = os.path.join(self.path, self.name, date + '.json' )
+        raw = self.get_argument('raw', '0')
+        fname = os.path.join(self.path, chan, date + '.json' )
         try:
             f = open(fname, 'r')
-            j = json.load(f)
-            for l in j:
-                if l.get('command'):
-                    if l['command'] == 'PRIVMSG':
-                        msg = '<%s> %s' % (l['nick'], l['message'])
-                    elif l['command'] == 'ACTION':
-                        msg = '*** %s %s' % (l['nick'], l['message'])
-                    elif l['command'] == 'JOIN':
-                        msg = '->| %s has joined %s' % (l['nick'], l['channel'])
-                    elif l['command'] == 'PART':
-                        msg = '|<- %s has left %s' % (l['nick'], l['channel'])
-                    elif l['command'] == 'QUIT':
-                        msg = '<-- %s has quit: %s' % (l['nick'], l['reason'])
-                    logs.append((l['time'] , msg))
+            if raw == '1':
+                self.write(f.read())
+                return
+            logs_json = json.load(f)
+            for log in logs_json:
+                if log.get('command'):
+                    if log['command'] == 'PRIVMSG':
+                        nick = log['nick']
+                        msg = log['message']
+                    elif log['command'] == 'ACTION':
+                        nick = log['nick']
+                        msg = '*** %s %s' % (log['nick'], log['message'])
+                    elif log['command'] == 'JOIN':
+                        nick = '-->'
+                        msg = '%s has joined %s' % (log['nick'], log['channel'])
+                    elif log['command'] == 'PART':
+                        nick = '<--'
+                        msg = '%s has left %s' % (log['nick'], log['channel'])
+                    elif log['command'] == 'QUIT':
+                        nick = '<--'
+                        msg = '%s has quit: %s' % (log['nick'], log['reason'])
+                    logs.append((log['time'] , nick, msg, log['command']))
 
             self.render('log.html', 
-                    title = self.name,
+                    title = chan,
                     subtitle = date,
                     logs = logs)
         except FileNotFoundError:
@@ -59,27 +102,28 @@ class ChanLogHandler(tornado.web.RequestHandler):
         else:
             f.close()
 
+
     def write_error(self, status_code, **kwargs):
         if status_code == 404:
-            self.render('log-no-found.html', 
-                    title = self.name,
-                    subtitle = self.path_args[0],
+            self.render('404.html', 
+                    title = self.path_args[0],
+                    subtitle = self.path_args[1],
+                    msg = 'There is no any log.'
                     )
 
+
 class Application(tornado.web.Application):
-
+    path = ''
+    chans = []
     def __init__(self, path):
-        chans = os.listdir(path)
+        self.path = path
+        self.chans = os.listdir(path)
 
-        urls = []
-        for chan in chans:
-           urls.append(tornado.web.url(
-               r'/%s/(today|\d{4}-\d{2}-\d{2})' % chan,
-               ChanLogHandler, 
-               { 'name': chan, 'path': path},
-               name = chan,
-               ))
-           handlers = [ tornado.web.url(r'/', IndexHandler, { 'chans': chans }) ] + urls
+        handlers = [
+                (r'/(.+)/(today|\d{4}-\d{2}-\d{2})', ChanLogHandler),
+                (r'/([^/]+)', ChanIndexHandler),
+                (r'/', IndexHandler),
+                ]
 
         settings = { 
             'template_path': os.path.join(os.path.dirname(__file__), 'templates'),
