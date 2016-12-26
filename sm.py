0766ea0b6cef1c29d14d23f5ea9b3b33d0fc08b2
Shengyu Zhang <silverrainz@outlook.com>
2016-12-27 01:44:27 +0800

    sm: Add subcommand for outputing all tags

--- sm.py
+++ sm.py
@@ -13,7 +13,7 @@ from bot import Bot
 
 class SMBot(Bot):
     targets = []
-    usage = '.sm <nick> [tag]; .LQYMGT; .lqymgt, .刘青云'
+    usage = '.sm[.<count>|.all] <nick> [tag]; i.e: .sm LQYMGT, .sm.2 LQYMGT .sm.all LQYMGT, .lqymgt, .lqymgtf .刘青云'
 
     n = 0
     data = {}
@@ -33,12 +33,17 @@ class SMBot(Bot):
     def on_LABOTS_MSG(self, target, bot, nick, msg):
         words = list(filter(lambda e: e, msg.split(' ', maxsplit = 2)))
 
+        # Command alias
         if words[0] in ['.LQYMGT', '.lqymgt', '.刘青云']:
             words = ['.sm', 'LQYMGT']
+        if words[0] in ['.LQYMGTF', '.lqymgtf']:
+            words = ['.sm.all', 'LQYMGT']
+        if words[0] in ['.small']:
+            words[0] = '.sm.all'
 
-        if words[0] == '.sm':
+        if words[0].startswith('.sm'):
             # Add
-            if words[2:] and words[2]:
+            if len(words) == 3:
                 name, tag = words[1], words[2]
                 if self.data.get(name):
                     if not tag in self.data[name]:
@@ -50,17 +55,33 @@ class SMBot(Bot):
                     self.data[name] = [tag]
                     self.say(target, '%s: Pushed!' % nick)
             # Query
-            elif words[1:]:
+            elif len(words) == 2:
+                subcmd = words[0][4:]
                 name = words[1]
-                if self.data.get(name):
+                try:
                     tags = self.data[name]
+                    if not tags:
+                        raise KeyError
+                except KeyError:
+                    self.say(target, '%s: No data' % nick)
+                    return
+
+                if subcmd:
+                    if subcmd == 'all':
+                        count = len(tags)
+                    else:
+                        try:
+                            count = min(int(subcmd), len(tags))
+                        except ValueError:
+                            count = 1
+                    for i in range(0, count):
+                        self.say(target, '%s: %s' % (nick,tags[i]))
+                else:
                     n = randint(0, len(tags) - 1)
                     while n == self.n and len(tags) != 1:
                         n = randint(0, len(tags) - 1)
                     self.n = n
                     self.say(target, '%s: %s' % (nick,tags[n]))
-                else:
-                    self.say(target, '%s: No data' % nick)
             else:
                 self.say(target, '%s: Usage: .sm <nick> [tag]' % nick)
 
